90a93ee3363ef0dc32adf3843bd3d9eedcf8a9fb
Malachi <malachi.habib@outlook.com>
2024-03-10 13:09:13 +1030

    init

--- src/pages/Home/Engagements.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Engagements = () => {
-  return <div>Engagements</div>;
-};
-
-export default Engagements;
--- src/states/index.js
+++
@@ -1,6 +0,0 @@
-import { proxy } from "valtio";
-
-export const states = proxy({
-  input: {},
-  selectedLink: "home",
-});
